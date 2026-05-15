97138ffa7df8a4f482f33f314452176fd918de5c
Andrea Peltokorpi <apeltoko@gmail.com>
2026-05-15 14:01:50 +0300

    fix: change printed filename

--- watch.js
+++ watch.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const PORT = 3456;
-const OUTPUT = path.join(__dirname, 'cv.pdf');
+const OUTPUT = path.join(__dirname, 'cv-andrea-peltokorpi.pdf');
 
 const MIME = {
   '.html': 'text/html',
